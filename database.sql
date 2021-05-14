50b200abb87eb16644efd912680659e78964f338
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-05-14 10:04:33 +0300

    yeni tablo düzeni

--- database.sql
+++ database.sql
@@ -1,22 +1,22 @@
+DROP DATABASE IF EXISTS kutuphanedb;
 CREATE DATABASE IF NOT EXISTS kutuphanedb;
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.adresler (
-	adres_no INT,
-	adres VARCHAR(45),
+	adres_no INT NOT NULL AUTO_INCREMENT,
+	adres VARCHAR(45) NOT NULL,
 	PRIMARY KEY (adres_no)
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.kutuphane (
-	kutuphane_no INT,
+	kutuphane_no INT NOT NULL AUTO_INCREMENT,
 	kutuphane_adi VARCHAR(45),
-	kutuphane_adres_no INT,
 	adres_no INT,
 	PRIMARY KEY (kutuphane_no),
 	FOREIGN KEY (adres_no) REFERENCES kutuphanedb.adresler(adres_no)
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.uyeler (
-	uye_no INT,
+	uye_no INT NOT NULL AUTO_INCREMENT,
 	uye_adi VARCHAR(45),
 	uye_soyadi VARCHAR(45),
 	uye_tel_no INT,
@@ -35,20 +35,20 @@ CREATE TABLE IF NOT EXISTS kutuphanedb.kitaplar (
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.yazarlar (
-	yazar_no INT,
+	yazar_no INT NOT NULL AUTO_INCREMENT,
 	yazar_adi VARCHAR(45),
 	yazar_soyadi VARCHAR(45),
 	PRIMARY KEY (yazar_no)
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.kategoriler (
-	kategori_no INT,
+	kategori_no INT NOT NULL AUTO_INCREMENT,
 	kategori_ismi VARCHAR(45),
 	PRIMARY KEY (kategori_no)
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.emanet (
-	emanet_no INT,
+	emanet_no INT NOT NULL AUTO_INCREMENT,
 	kutuphane_no INT,
 	uye_no INT,
 	isbn_no INT,
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS kutuphanedb.emanet (
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.kitap_kategori (
-	kitap_kategori_no INT,
+	kitap_kategori_no INT NOT NULL AUTO_INCREMENT,
 	isbn_no INT,
 	kategori_no INT,
 	PRIMARY KEY (kitap_kategori_no),
@@ -70,10 +70,19 @@ CREATE TABLE IF NOT EXISTS kutuphanedb.kitap_kategori (
 );
 
 CREATE TABLE IF NOT EXISTS kutuphanedb.kitap_yazar (
-	kitap_yazar_no INT,
+	kitap_yazar_no INT NOT NULL AUTO_INCREMENT,
 	isbn_no INT,
 	yazar_no INT,
 	PRIMARY KEY (kitap_yazar_no),
 	FOREIGN KEY (isbn_no) REFERENCES kutuphanedb.kitaplar(isbn_no),
 	FOREIGN KEY (yazar_no) REFERENCES kutuphanedb.yazarlar(yazar_no)
+);
+
+CREATE TABLE IF NOT EXISTS kutuphanedb.kutuphane_kitap (
+	kutuphane_kitap_no INT NOT NULL AUTO_INCREMENT,
+	kutuphane_no INT,
+	isbn_no INT,
+	PRIMARY KEY (kutuphane_kitap_no),
+	FOREIGN KEY (kutuphane_no) REFERENCES kutuphanedb.kutuphane(kutuphane_no),
+	FOREIGN KEY (isbn_no) REFERENCES kutuphanedb.kitaplar(isbn_no)
 );
